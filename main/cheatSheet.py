fe29069bad677d56e702a34ada9398e7ca13f7f4
danielberrones <daniel.a.berrones@gmail.com>
2022-01-26 22:34:53 -0600

    refactored

--- main/cheatSheet.py
+++ main/cheatSheet.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 pd.set_option('max_rows',None)
-df = pd.read_csv('./data/bankData.csv',sep=';')
+df = pd.read_csv('../data/bankData.csv',sep=';')
 
 print('df.describe()')
 print(df.describe())
